760106e4cb7d99a4492c0d287acd30d78a60ce90
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2024-01-26 11:30:30 +0100

    rter

--- MemoriaProjekt/index.js
+++ MemoriaProjekt/index.js
@@ -7,11 +7,25 @@ function init(){
 
 function kepKirakas(){
     let asztal=document.getElementById("asztal");
+    for(let k=0;k<2;k++)    
+        for(let i=0;i<kepekUrl.length;i++){
+            /*
+            let uj=document.createElement("img");
 
-    for(let i=0;i<kepekUrl.length;i++){
-        let uj=document.createElement("img");
-        uj.src="kepek/"+ kepekUrl[i];
-        asztal.appendChild(uj);
-    }
+
+            uj.src="kepek/"+ kepekUrl[i];
+            asztal.appendChild(uj);
+            */
+            let uj=document.createElement("div");
+
+            uj.className="kartya"; //div class="kartya"
+            uj.style.backgroundImage="url(kepek/"+ kepekUrl[i]+")";
+            asztal.appendChild(uj);
+            
+        }
    
+}
+
+function kever(){
+    
 }
