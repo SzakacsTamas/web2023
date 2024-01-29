07d263738343642538267c8a596f993e6673043b
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2024-01-29 14:02:55 +0100

    aqeew

--- MemoriaProjekt/index.js
+++ MemoriaProjekt/index.js
@@ -1,31 +1,55 @@
 var kepekUrl=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"]
 
 
-function init(){
+var parDb = 4;
+var kattintas=0
+
+function init()
+{
     kepKirakas();
 }
 
-function kepKirakas(){
-    let asztal=document.getElementById("asztal");
-    for(let k=0;k<2;k++)    
-        for(let i=0;i<kepekUrl.length;i++){
-            /*
-            let uj=document.createElement("img");
+function kepKirakas()
+{
+    
+    let asztal = document.getElementById("asztal");
 
+    let kartyak = [];
 
-            uj.src="kepek/"+ kepekUrl[i];
+    for(let k = 0; k < 2; k++)
+    {
+        for(let i=0; i < parDb;i++)
+        {
+            /*let uj = document.createElement("img");
+            uj.src="kepek/" + kepekUrl[i];
             asztal.appendChild(uj);
             */
-            let uj=document.createElement("div");
 
-            uj.className="kartya"; //div class="kartya"
-            uj.style.backgroundImage="url(kepek/"+ kepekUrl[i]+")";
-            asztal.appendChild(uj);
-            
+            let uj = document.createElement("div");
+            uj.className="kartya";
+            uj.onclick=function(){uj.style.backgroundImage="url(kepek/" + kepekUrl[i] + ")"};
+            //asztal.appendChild(uj);
+            kartyak.push(uj);
+
         }
-   
+    }
+    kartyak=kever(kartyak);
+
+    for(let i=0; i < kartyak.length; i++)
+    {
+    asztal.appendChild(kartyak[i]);
+    }
+    
 }
 
-function kever(){
-    
+function kever(points) {
+
+    for (let i = points.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i+1));
+      let k = points[i];
+      points[i] = points[j];
+      points[j] = k;
+    }
+
+    return points;
 }
