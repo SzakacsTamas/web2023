e40bacc9557c6c0ed591b72dd2fdafe64e8180ad
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-02-07 12:35:15 +0100

    projektora

--- MemoriaProjekt/index.js
+++ MemoriaProjekt/index.js
@@ -1,55 +1,116 @@
-var kepekUrl=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"]
+var kepekurl = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
 
-
-var parDb = 4;
-var kattintas=0
-
+];
+var pardb = 4;
+var kattintas = 0;
 function init()
 {
-    kepKirakas();
+    kepkirakas();
 }
-
-function kepKirakas()
+function kepkirakas()
 {
-    
     let asztal = document.getElementById("asztal");
 
     let kartyak = [];
 
-    for(let k = 0; k < 2; k++)
+    for(let j = 0; j < 2; j++)
     {
-        for(let i=0; i < parDb;i++)
+        for(let i = 0; i < pardb; i++)
         {
-            /*let uj = document.createElement("img");
-            uj.src="kepek/" + kepekUrl[i];
-            asztal.appendChild(uj);
-            */
-
             let uj = document.createElement("div");
-            uj.className="kartya";
-            uj.onclick=function(){uj.style.backgroundImage="url(kepek/" + kepekUrl[i] + ")"};
-            //asztal.appendChild(uj);
+            uj.className = "kartya";
+            uj.onclick = function(){
+                if (kattintas < 2 &&
+                     typeof uj.dataset.felforditva!=="undifined"&&  uj.dataset.felforditva!="true")
+                {
+                    uj.style.backgroundImage = "url(kepek/" + kepekurl[i] + ")";
+                    uj.dataset.felforditva = "true";
+                   
+                    kattintas++;
+                }
+                
+                if (kattintas === 2)
+                {
+                    setTimeout(visszafordit, 2000);
+                }
+            };
+            
             kartyak.push(uj);
-
         }
     }
-    kartyak=kever(kartyak);
 
-    for(let i=0; i < kartyak.length; i++)
+    kartyak = kever(kartyak);
+
+    for(let i = 0; i < kartyak.length; i++)
     {
-    asztal.appendChild(kartyak[i]);
+        asztal.appendChild(kartyak[i]);
     }
+}   
+
+function visszafordit()
+{
+    const lapok = document.getElementById("asztal").children;
+    const aktiv = [];
+    for (let i = 0; i < lapok.length; i++)
+    {
+        if (lapok[i].dataset.felforditva === "true")
+        {
+            aktiv.push(lapok[i]);
+        }
+    }
+    if (aktiv[0].style.backgroundImage !== aktiv[1].style.backgroundImage)
+    {
+        aktiv[0].style.backgroundImage = "";
+        aktiv[1].style.backgroundImage = "";
+    }
+    else
+    {
+        aktiv[0].onclick = "";
+        aktiv[1].onclick = "";
+        if (!vanemeg())
+        {
+            nyertel();
+        }
+    }
+    aktiv[0].dataset.felforditva = "";
+    aktiv[1].dataset.felforditva = "";
+
+    kattintas = 0;
+}
+
+function nyertel()
+{
+    let uj = doucment.createElement("div");
+    uj.innerHTML = "Game over";
+
     
 }
 
-function kever(points) {
-
-    for (let i = points.length -1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i+1));
-      let k = points[i];
-      points[i] = points[j];
-      points[j] = k;
+function vanemeg()
+{
+    let db = 0;
+    const lapok = document.getElementById("asztal").children;
+    for (let i = 0; i < lapok.length; i++)
+    {
+        if (lapok[i].style.backgroundImage === "")
+        {
+            db++;
+        }
     }
+    return db > 0;
+}
 
-    return points;
+function kever(points) 
+{
+  for (let i = points.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i+1));
+    let k = points[i];
+    points[i] = points[j];
+    points[j] = k;
+  }
+  return points;
 }
