2120dbce3638802523ec834c1605f07d1ea9feaa
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-02-29 08:43:09 +0100

    hkgfh

--- Gzadlakodj/index.js
+++ Gzadlakodj/index.js
@@ -1,13 +1,16 @@
 class jatekos {
-    constructor(nev, karakter,penz,mezo) {
+    constructor(nev, karakter,penz) {
       this.nev = nev;
       this.karakter = karakter;
       this.penz=penz;
-      this.mezo=mezo;
+      
     }
   }
 
-  const Tomi=new jatekos(Tomi,macska,,1)
-  const Petya=new jatekos(Tomi,macska,,1)
-  const Gabi=new jatekos(Tomi,macska,,1)
-  const Jazmin=new jatekos(Tomi,macska,,1)
+
+
+
+  let Tomi=new jatekos(Tomi,figura1,75000)
+  let Petya=new jatekos(Petya,figura2,75000)
+  let Gabi=new jatekos(Gabi,figura3,75000)
+  let Jazmin=new jatekos(Jazmin,figura4,75000)
