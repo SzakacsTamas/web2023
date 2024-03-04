226209c4251748986e9907a6bed96f7717e7230c
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-03-04 13:19:59 +0100

    ghfghgf

---
+++ ADATBÁZIS/NB/asddddd.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE nb1 DEFAULT CHARACTER set=utf8 Collate utf8_hungarian_ci;
+
+USE nb1;
+
+SELECT vezeteknev, utonev, FROM labdarugo WHERE magyar AND kulfoldi;
+
+CREATE TABLE `klub` (
+  `id` int(11) NOT NULL,
+  `csapatnev` varchar(100) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+CREATE TABLE `labdarugo` (
+  `id` int(11) NOT NULL,
+  `mezszam` int(11) NOT NULL,
+  `klubid` int(11) NOT NULL,
+  `posztid` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `utonev` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `vezeteknev` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `szulido` date NOT NULL,
+  `magyar` tinyint(1) NOT NULL,
+  `kulfoldi` tinyint(1) NOT NULL,
+  `ertek` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+CREATE TABLE `poszt` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(100) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+ALTER TABLE `klub`
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `labdarugo`
+  ADD PRIMARY KEY (`id`);
+  
+ ALTER TABLE `poszt`
+  ADD PRIMARY KEY (`id`);
