85528a5b60b124f8048b97a276eae2ddc8a3d918
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2024-04-07 20:45:38 +0200

    gfd

---
+++ Statuszkodok/index.js
@@ -0,0 +1,57 @@
+function x1(){
+    alert("Continue")
+}
+function x2(){
+    alert("Switching Protocols")
+}
+function x3(){
+    alert("Processing")
+}
+function xx1(){
+    alert("OK")
+}
+function xx2(){
+    alert("Created")
+}
+function xx3(){
+    alert("Accepted")
+}
+function xx4(){
+    alert("Non-Authoritative Information")
+}
+function xx5(){
+    alert("No Content")
+}
+function xx6(){
+    alert("Reset Content")
+}
+function xx7(){
+    alert("Partial Content")
+}
+function xx8(){
+    alert("Multi-Status ")
+}
+function xx9(){
+    alert("Already Reported")
+}
+function xx26(){
+    alert("IM Used")
+}function xxx1(){
+    alert("Unauthorized")
+}function xxx2(){
+    alert("Payment Required")
+}function xxx3(){
+    alert("Forbidden")
+}function xxx4(){
+    alert("Not Found")
+}function xxx5(){
+    alert("Method Not Allowed")
+}function xxx6(){
+    alert("Not Acceptable")
+}function xxx7(){
+    alert("Proxy Authentication Required")
+}function xxx8(){
+    alert("Request Timeout")
+}function xxx9(){
+    alert("Conflict")
+}
