b103c422e65bd998ae8f6e582a98b35f446152b6
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-04-10 14:04:54 +0200

    fdsfg

---
+++ ADATBÁZIS/Operett/new 1.sql
@@ -0,0 +1,39 @@
+\C latin2
+CREATE DATABASE operett DEFAULT CHARSET utf8 COLLATE utf8_hungarian_ci;
+
+
+
+USE operett;
+
+CREATE TABLE `kapcsolat` (
+  `id` int(11) NOT NULL,
+  `muid` int(11) NOT NULL,
+  `tipus` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `alkotoid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+CREATE TABLE `alkoto` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(100) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+
+CREATE TABLE `mu` (
+  `id` int(11) NOT NULL,
+  `cim` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `eredeti` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `szinhaz` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `ev` int(11) NOT NULL,
+  `felvonas` int(11) NOT NULL,
+  `kep` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+ALTER TABLE `kapcsolat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1561;
+/*ADATOK*/
+
+SELECT cim FROM mu WHERE ev=1916 ORDER BY cim;SELECT nev,tipus FROM mu join kapcsolat ON mu.id=muid join alkoto on alkotoid=alkoto.id WHERE cim="Bob herceg";
+
+SELECT ev FROM mu ORDER by ev DESC limit 1;
+
+SELECT nev,COUNT(muid) FROM alkoto join kapcsolat on alkotoid=alkoto.id GROUP by alkotoid
