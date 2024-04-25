904e07cda5db7a3b004908e349b91c541fb27c2d
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-04-25 14:04:50 +0200

    gfgfds

--- ADATBÁZIS/Diafilmek/diafilmek.sql
+++ ADATBÁZIS/Diafilmek/diafilmek.sql
@@ -6,3 +6,32 @@ USE diafilmek;
 /*tabla*/
 
 /*adatok*/
+
+/*
+2.feladat 
+
+*/
+SELECT cim, kiadasnev FROM film WHERE kiadasnev > 2000 ORDER BY cim;
+
+/*3 Feladat*/
+SELECT cim,kocka,kiadasiev FROM film WHERE cim LIKE "%farkas%";
+/*azért kell a %% hogyha bárhol előfordul*/
+
+/*4 feladat*/
+SELECT DISTINCT nev FROM film JOIN kiado on kiadoid=kiado.id 
+WHERE cim LIKE "%sicc%";
+/*DISTINCT az hogy csak egyszer szerepeljen*/
+
+/*5 feladat*/
+SELECT cim,COUNT(*) FROM film GROUP BY cim ORDER BY 2 DESC LIMIT 1;
+/*DESC csokkeno*/
+/* 6 feladat*/
+SELECT kiadasiev,COUNT(*) FROM film WHERE kiadasiev!=0 GROUP BY kiadasiev ORDER BY 2 DESC;
+/*<> NEM Egyenlő*/
+
+/*7 feladat*/
+SELECT DISTINCT film.cim FROM film JOIN film AS film2 ON film.cim=film2.cim
+ WHERE film.szinese AND NOT film2.szinese;
+
+
+
